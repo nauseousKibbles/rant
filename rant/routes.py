7bbaa352848f1944d004eed8333036073c4af4ab
Greyson <nauseouskibbles@pop-os.localdomain>
2019-11-17 13:27:22 -0600

    I changed more css and finished python form stuff

--- rant/routes.py
+++ rant/routes.py
@@ -1,8 +1,11 @@
 from rant import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from rant.forms import rantform
 
 @app.route("/", methods=['GET','POST'])
 def home():
     form = rantform()
+    if form.validate_on_submit():
+        flash('You submitted your rant.', 'success')
+        return redirect(url_for('home'))
     return render_template("home.html", form=form)
